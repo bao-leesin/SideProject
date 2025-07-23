a2b6a65c797bc862b60a6d4536c6730c6c617732
baohdt <baohd@vtc.vn>
2025-07-23 13:57:07 +0700

    cbi cho triển khai dự án mới

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -10,20 +10,11 @@ namespace Data
         {
         }
 
-        // DbSet for ACCOUNT entity
-        public DbSet<ACCOUNT> ACCOUNTs { get; set; }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
-            // Configure the ACCOUNT entity
-            modelBuilder.Entity<ACCOUNT>(entity =>
-            {
-                entity.HasKey(e => e.USERNAME);
-                entity.Property(e => e.USERNAME).IsRequired().HasMaxLength(50);
-                entity.Property(e => e.PASSWORD).IsRequired().HasMaxLength(50);
-            });
+           
         }
     }
 }
